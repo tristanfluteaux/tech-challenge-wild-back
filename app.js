1137fa9ebfbaa7d4f18ce55670d23b9c98dc8a07
Tristan <tristan.fluteaux@gmail.com>
2021-11-01 16:22:02 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+const connection = require('./db-config.js')
+
+const app = express()
+
+const port = process.env.PORT || 4000
+
+connection.connect(err => {
+    if (err) {
+      console.error('error connecting: ' + err.stack)
+    } else {
+      console.log('connected to database with threadId :  ' + connection.threadId)
+    }
+  })
+
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+  })
